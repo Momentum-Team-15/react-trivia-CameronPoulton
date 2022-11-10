987b1865427f7ebd96d64b26aa09b86740ac305f
Cameron Poulton <Crp2108@hotmail.com>
2022-11-10 14:11:32 -0500

    questions

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Categories } from './components/Categories';
+import { Questions } from './components/Questions';
+
+
+function App() {
+  const [category, setCategory] = useState([])
+  const [SelectedCategoryId, setSelectedCategoryId] = useState([])
+  // const [questions, setQuestions] = useState([])
+  const [Url, setUrl] = useState([])
+
+  useEffect(() => {
+    axios.get('https://opentdb.com/api_category.php').then(res => setCategory(res.data.trivia_categories))
+  }, [])
+  console.log(category)
+  return (
+    <section className='trivia-container'>
+      <header><h1>Trivia Game</h1></header>
+        {SelectedCategoryId ? (
+          <Questions
+            selectedCategoryId={setSelectedCategoryId}
+            setSelectedCategoryId={setSelectedCategoryId} />
+            ) : (
+            <>  
+            {category &&
+              <Categories
+                setSelectedCategoryId={setSelectedCategoryId}
+                category={category}
+                setUrl={setUrl}/>}
+            </>
+          )}
+      </section>
+  )};
+
+
+export default App;
